95d669f0a5d82130a2ea58e104b7fdac9235c82e
DiverseRaul <goraulseixas@gmail.com>
2025-02-18 11:12:06 -0500

    more

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     particlesJS('particles-js', {
         particles: {
             number: {
-                value: 80,
+                value: 100,
                 density: {
                     enable: true,
                     value_area: 800
